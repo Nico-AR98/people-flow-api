e4400d27138d71127e234681a4d891e008f1da00
Nico Gallardo <alfredongallg@gmail.com>
2025-09-29 01:56:04 -0300

    modified employee serializer

--- employees/api/serializers.py
+++ employees/api/serializers.py
@@ -3,9 +3,14 @@ from employees.models import Employee, JobPosition
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    job_position = serializers.SerializerMethodField('get_job_position')
+
+    def get_job_position(self, obj):
+        return obj.job_position.title
+
     class Meta:
         model = Employee
-        fields = "__all__"
+        fields = ['name', 'surname', 'email', 'job_position', 'salary', 'hire_date']
 
 
 class JobPositionSerializer(serializers.ModelSerializer):
